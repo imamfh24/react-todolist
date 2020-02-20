8c680219cc52e37aab49fc68a7defd0f31b12c9d
IFA <imamfh24@gmail.com>
2020-02-20 15:15:30 +0700

    membagi kecil komponen

--- src/App.js
+++ src/App.js
@@ -2,34 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Paper from "./component/Paper";
+import Header from "./component/Header";
+import TodoForm from "./component/TodoForm";
+import Todos from "./component/Todos";
 
 function App() {
   return (
     <Paper>
-                        <section className="header">
-                            <button className="header-btn main-black-color">Add</button>
-                            <h1 className="header-title">Todo Lists</h1>
-                            <button className="header-btn main-red-color">Clear</button>
-                        </section>
-                        <section className="add">
-                            <form className="add-form" action>
-                                <input type="text" className="add-input" />
-                                <button className="add-btn main-black-color">Add</button>
-                            </form>
-                        </section>
-                        <section className="todos">
-                            <div className="todo">
-                                <span className="todo-text">Learning React</span>
-                            </div>
-                        </section>
-                        <section className="todos">
-                            <div className="todo">
-                                <span className="todo-text">Learning React</span>
-                            </div>
-                        </section>
-                        <section className="todos">
-                        </section>
-                        </Paper>
+      <Header/>
+      <TodoForm/>
+      <Todos/>
+    </Paper>
   );
 }
 
